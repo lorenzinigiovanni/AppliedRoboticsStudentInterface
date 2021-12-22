b9100c9b286c1bb93d7681702e18e1bfeb660a5b
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2021-12-22 09:27:36 +0100

    Test new methods in our test bench

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,35 @@
 #include "../../simulator/src/9_project_interface/include/utils.hpp"
 #include "clipper/clipper.hpp"
-
-#include <stdexcept>
-#include <sstream>
-
-class LineOffsetter
-{
-public:
-    static std::vector<Polygon> offset_obstacles(const std::vector<Polygon> &obstacles)
-    {
-        std::vector<Polygon> result;
-        for (const auto &obstacle : obstacles)
-        {
-            result.push_back(offset_obstacle(obstacle));
-        }
-        return result;
-    }
-
-    static Polygon offset_obstacle(Polygon polygon)
-    {
-        ClipperLib::Path subj;
-        ClipperLib::Paths solution;
-
-        // convert Poligons into path
-        for (int i = 0; i < polygon.size(); i++)
-        {
-            subj.push_back(convert_point_to_int_point(polygon[i]));
-        }
-
-        ClipperLib::ClipperOffset co;
-        co.AddPath(subj, ClipperLib::jtMiter, ClipperLib::etClosedPolygon);
-        co.Execute(solution, 100); // Quanto sta da offsettare
-
-        Polygon result;
-
-        for (int i = 0; i < solution[0].size(); i++)
-        {
-            result.push_back(convert_int_point_to_point(solution[0][i]));
-        }
-
-        return result;
-    }
-
-    static ClipperLib::IntPoint convert_point_to_int_point(Point _p)
-    {
-        ClipperLib::IntPoint p;
-        p.X = _p.x * 1000;
-        p.Y = _p.y * 1000;
-        return p;
-    }
-
-    static Point convert_int_point_to_point(ClipperLib::IntPoint _ip)
-    {
-        Point p;
-        p.x = float(_ip.X) / 1000;
-        p.y = float(_ip.Y) / 1000;
-        return p;
-    }
-};
+#include "line_offsetter.hpp"
+#include "cell_decomposition.hpp"
+#include <iostream>
 
 int main()
 {
-    std::vector<Polygon> obstacles;
-    obstacles.push_back(Polygon({Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0)}));
-    obstacles.push_back(Polygon({Point(2, 4), Point(10, 3), Point(4, 2), Point(7, 4)}));
+    Polygon border = Polygon({Point(0, 0), Point(0, 1), Point(0.8, 1), Point(0.8, 0)});
 
-    std::vector<Polygon> offsetted_obstacle_list = LineOffsetter::offset_obstacles(obstacles);
-    
+    std::vector<Polygon> obstacles;
+    obstacles.push_back(Polygon({Point(0.2, 0.2), Point(0.2, 0.4), Point(0.4, 0.4), Point(0.4, 0.2)}));
+    obstacles.push_back(Polygon({Point(0.35, 0.35), Point(0.35, 0.65), Point(0.65, 0.65), Point(0.65, 0.35)}));
+    obstacles.push_back(Polygon({Point(0.6, 0.6), Point(0.6, 0.75), Point(0.75, 0.75), Point(0.75, 0.6)}));
+    // obstacles.push_back(Polygon({Point(0.2, 0.4), Point(0.2, 0.3), Point(0.4, 0.2), Point(0.1, 0.4)}));
+
+    std::vector<Polygon> borders;
+    borders.push_back(border);
+
+    std::vector<Polygon> offsetted_borders = LineOffsetter::offset_polygons(borders, -50);
+    std::vector<Polygon> offsetted_obstacles = LineOffsetter::offset_polygons(obstacles, 50);
+
+    std::vector<Polygon> merged_paths = LineOffsetter::merge_polygons(offsetted_obstacles);
+    std::vector<Polygon> intersected_paths_borders = LineOffsetter::intersect_polygons(offsetted_borders, merged_paths);
+
+    // cell decomposition
+    CellDecomposition cell_decomposition;
+    cell_decomposition.add_polygons(offsetted_borders);
+    cell_decomposition.add_polygons(intersected_paths_borders);
+    cell_decomposition.create_cdt();
+    cell_decomposition.print_triangles();
+    cell_decomposition.show_triangles();
+
     return 0;
 }
