b87c3b6dd3ec52c21f964de1e3abaea762738112
Simone Luchetta <simone.luchetta@studenti.unitn.it>
2021-12-30 14:34:22 +0100

    Add iterative algorithm to do Multipoint Dubins
    Co-authored-by: Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
    Co-authored-by: Diego Planchenstainer <d.planchenstainer@studenti.unitn.it>

--- src/router.hpp
+++ src/router.hpp
@@ -5,6 +5,7 @@
 #include "dubins/dpoint.hpp"
 #include "dubins/dubins.hpp"
 #include <vector>
+#include <cmath>
 
 class Router
 {
@@ -13,7 +14,8 @@ private:
     std::vector<dPoint> dpoints;
 
     double k_max = 25;
-    double n_angle_steps = 36.0;
+    int n_angle_steps = 32;
+    int iterations = 2;
 
 public:
     void add_path(std::vector<Point> &path, float theta_start)
@@ -28,48 +30,116 @@ public:
         double angle_step = angle / n_angle_steps;
 
         std::vector<std::vector<double>> lenghts_m(path_lenght, std::vector<double>(n_angle_steps, 10000.0)); // matrix path_lenght x n_angle_steps
-        std::vector<std::vector<int>> angles_m(path_lenght, std::vector<int>(n_angle_steps, 0));              // matrix path_lenght x n_angle_steps
 
-        for (int n = path_lenght - 2; n >= 0; n--)
+        std::vector<std::vector<int>> angles_m(path_lenght, std::vector<int>(n_angle_steps, 0)); // matrix path_lenght x n_angle_steps
+        std::vector<std::vector<int>> angles_i(iterations, std::vector<int>(path_lenght, 0));    // matrix iterations x path_lenght
+
+        for (int iteration = 0; iteration < iterations; iteration++)
         {
-            for (int i = 0; i < n_angle_steps; i++)
+            for (int n = path_lenght - 2; n >= 0; n--)
             {
-                for (int j = 0; j < n_angle_steps; j++)
+                for (int i = 0; i < n_angle_steps; i++)
                 {
-                    dPoint point_1(path[n], angle_step * i);
-                    dPoint point_2(path[n + 1], angle_step * j);
-
-                    Dubins::Solution solution = Dubins::solve(point_1, point_2, k_max);
-                    double l1 = solution.c.L;
-
-                    double l2 = 0.0;
-                    if (n != path_lenght - 2)
+                    for (int j = 0; j < n_angle_steps; j++)
                     {
-                        l2 = lenghts_m[n + 1][j];
-                    }
+                        double angle_1 = 0.0;
+                        double angle_2 = 0.0;
 
-                    if (lenghts_m[n][i] > l1 + l2)
-                    {
-                        lenghts_m[n][i] = l1 + l2;
-                        angles_m[n + 1][i] = j;
+                        for (int z = 0; z < iteration; z++)
+                        {
+                            angle_1 += angle_step * angles_i[z][n] * std::pow(3.0 / n_angle_steps, z);
+                            angle_2 += angle_step * angles_i[z][n + 1] * std::pow(3.0 / n_angle_steps, z);
+
+                            if (z > 0)
+                            {
+                                angle_1 -= angle_step * (3.0 / 2.0) * std::pow(3.0 / n_angle_steps, z - 1);
+                                angle_2 -= angle_step * (3.0 / 2.0) * std::pow(3.0 / n_angle_steps, z - 1);
+                            }
+                        }
+
+                        angle_1 += angle_step * i * std::pow(3.0 / n_angle_steps, iteration);
+                        angle_2 += angle_step * j * std::pow(3.0 / n_angle_steps, iteration);
+
+                        if (iteration > 0)
+                        {
+                            angle_1 -= angle_step * (3.0 / 2.0) * std::pow(3.0 / n_angle_steps, iteration - 1);
+                            angle_2 -= angle_step * (3.0 / 2.0) * std::pow(3.0 / n_angle_steps, iteration - 1);
+                        }
+
+                        dPoint point_1(path[n], angle_1);
+                        dPoint point_2(path[n + 1], angle_2);
+
+                        Dubins::Solution solution = Dubins::solve(point_1, point_2, k_max);
+                        double l1 = solution.c.L;
+
+                        double l2 = 0.0;
+                        if (n != path_lenght - 2)
+                        {
+                            l2 = lenghts_m[n + 1][j];
+                        }
+
+                        if (lenghts_m[n][i] > l1 + l2)
+                        {
+                            lenghts_m[n][i] = l1 + l2;
+                            angles_m[n + 1][i] = j;
+                        }
                     }
                 }
             }
-        }
 
-        int prev_angle = (int)(theta_start / angle_step);
+            std::vector<int> prev_angle(iterations);
+
+            prev_angle[0] = round(theta_start / angle_step);
+
+            for (int i = 1; i < iteration; i++)
+            {
+                int angle = (int)round(theta_start / angle_step);
+                for (int j = 0; j < i; j++)
+                {
+                    angle += -prev_angle[j] * pow(3 / angle_step, j) + (3.0 / 2.0) * pow(3 / angle_step, j);
+                }
+
+                if (angle >= n_angle_steps)
+                {
+                    angle = 0;
+                }
+                else if (angle < 0)
+                {
+                    angle = 0;
+                }
+
+                prev_angle[i] = angle;
+            }
+
+            for (int i = 0; i < path_lenght; i++)
+            {
+                int curr_angle = angles_m[i][prev_angle[iteration]];
+                angles_i[iteration][i] = curr_angle;
+                prev_angle[iteration] = curr_angle;
+            }
+        }
 
         for (int i = 0; i < path.size(); i++)
         {
             if (i == 0)
             {
-                dpoints.push_back(dPoint(path[i], prev_angle * angle_step));
+                dpoints.push_back(dPoint(path[i], theta_start));
             }
             else
             {
-                int curr_angle = angles_m[i][prev_angle];
-                dpoints.push_back(dPoint(path[i], curr_angle * angle_step));
-                prev_angle = curr_angle;
+                double angle = 0.0;
+
+                for (int iteration = 0; iteration < iterations; iteration++)
+                {
+                    angle += angle_step * angles_i[iteration][i] * std::pow(3.0 / n_angle_steps, iteration);
+
+                    if (iteration > 0)
+                    {
+                        angle -= angle_step * (3.0 / 2.0) * std::pow(3.0 / n_angle_steps, iteration - 1);
+                    }
+                }
+
+                dpoints.push_back(dPoint(path[i], angle));
             }
         }
     }
