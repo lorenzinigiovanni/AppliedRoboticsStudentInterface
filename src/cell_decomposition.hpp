4e55a70627a6b1e9a91aabc2a0187f92af322809
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2022-01-03 15:26:39 +0100

    Add comments to cell decomposition
    Co-authored-by: Diego Planchenstainer <d.planchenstainer@studenti.unitn.it>
    Co-authored-by: Simone Luchetta <simone.luchetta@studenti.unitn.it>

--- src/cell_decomposition.hpp
+++ src/cell_decomposition.hpp
@@ -11,12 +11,22 @@
 #include <stdexcept>
 #include <sstream>
 
+/**
+ * @brief Edge between two vertexes
+ *
+ */
 class Edge
 {
 public:
-    std::size_t v1;
-    std::size_t v2;
+    std::size_t v1; // first vertex
+    std::size_t v2; // second vertex
 
+    /**
+     * @brief Construct a new Edge object
+     *
+     * @param _v1 First vertex
+     * @param _v2 Second vertex
+     */
     Edge(std::size_t _v1, std::size_t _v2)
     {
         v1 = _v1;
@@ -24,50 +34,66 @@ public:
     }
 };
 
+/**
+ * @brief Object for performing triangular cell decomposition
+ *
+ */
 class CellDecomposition
 {
 private:
-    CDT::Triangulation<float> cdt;
-    std::size_t point_count = 0;
+    CDT::Triangulation<float> cdt; // cdt library object
+    std::size_t point_count = 0;   //
 
 public:
-    std::vector<Point> points;
-    std::vector<Edge> edges;
-    std::vector<CDT::Triangle> triangles;
+    std::vector<Point> points;            // vertexes of the polygons
+    std::vector<Edge> edges;              // edges between the polygons vertexes
+    std::vector<CDT::Triangle> triangles; // triangles found by applying triangular decomposition
 
+    /**
+     * @brief Insert a polygon
+     *
+     * @param polygon A polygon
+     */
     void add_polygon(const Polygon &polygon)
     {
+        // for each point in the polygon
         for (int i = 0; i < polygon.size(); i++)
         {
+            // add the vertex point
             points.push_back(polygon[i]);
             point_count++;
+
+            // add the edges
             if (i > 0)
             {
                 edges.push_back(Edge(point_count - 1, point_count - 2));
             }
         }
+
+        // add the last closing edge
         edges.push_back(Edge(point_count - 1, point_count - polygon.size()));
     }
 
+    /**
+     * @brief Insert polygons
+     *
+     * @param polygons A vector of Polygon
+     */
     void add_polygons(const std::vector<Polygon> &polygons)
     {
         for (int i = 0; i < polygons.size(); i++)
         {
-            for (int j = 0; j < polygons[i].size(); j++)
-            {
-                points.push_back(polygons[i][j]);
-                point_count++;
-                if (j > 0)
-                {
-                    edges.push_back(Edge(point_count - 1, point_count - 2));
-                }
-            }
-            edges.push_back(Edge(point_count - 1, point_count - polygons[i].size()));
+            add_polygon(polygons[i]);
         }
     }
 
+    /**
+     * @brief Create a cdt library object
+     *
+     */
     void create_cdt()
     {
+        // insert vertex in the cdt library object
         cdt.insertVertices(
             points.begin(),
             points.end(),
@@ -76,6 +102,7 @@ public:
             [](const Point &p)
             { return p.y; });
 
+        // insert edges in the cdt library object
         cdt.insertEdges(
             edges.begin(),
             edges.end(),
@@ -84,12 +111,19 @@ public:
             [](const Edge &e)
             { return e.v2; });
 
+        // remove the big triangle around all the other
         // cdt.eraseSuperTriangle();
+
+        // remove the big triangle and remove the edges inside the obstacles
         cdt.eraseOuterTrianglesAndHoles();
 
         triangles = cdt.triangles;
     }
 
+    /**
+     * @brief Print the triangles in console
+     *
+     */
     void print_triangles()
     {
         for (CDT::TriangleVec::iterator t = triangles.begin(); t != triangles.end(); ++t)
@@ -98,9 +132,13 @@ public:
         }
     }
 
+    /**
+     * @brief Print the triangles on the image
+     *
+     * @param img The image to add the triangles to
+     */
     void show_triangles(cv::Mat &img)
     {
-
         for (CDT::TriangleVec::iterator t = triangles.begin(); t != triangles.end(); ++t)
         {
             for (int i = 0; i < 3; i++)
